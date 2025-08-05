f6a09f5fc8e8f14a24eac11c36b2f5ebfa9aefeb
JulWas797 <51297298+JulWas797@users.noreply.github.com>
2025-08-05 15:53:46 +0200

    Optimizations, TRIM

--- configuration.nix
+++ configuration.nix
@@ -50,7 +50,23 @@
     nerd-fonts.fira-code
   ];
 
+  services.fstrim.enable = true;
+
   system.stateVersion = "25.05";
   nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nix = {
+    optimise.automatic = true;
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
 }
