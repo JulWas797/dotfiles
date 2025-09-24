cb83fdf0e17a83d121a3cd4e740764121a999986
JulWas797 <51297298+JulWas797@users.noreply.github.com>
2025-09-24 13:30:09 +0200

    zsh-nix-shell (janky)

--- user/zsh.nix
+++ user/zsh.nix
@@ -1,5 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  agnoster-nix-theme = builtins.fetchurl {
+    url = "https://gist.githubusercontent.com/chisui/0d12bd51a5fd8e6bb52e6e6a43d31d5e/raw/a97b74ce17c5f1befabe266ccf02a972cab2911b/agnoster-nix.zsh-theme";
+    sha256 = "1m7qqrp8z0glnq81c9ldzmm0r42rgdmw8nk9hvssbjphx5khk6z7";
+  };
+in
 {
   programs.zsh = {
     enable = true;
@@ -11,7 +17,7 @@
 
     oh-my-zsh = {
       enable = true;
-      theme = "agnoster";
+      theme = "agnoster-nix";
 
       plugins = [
         "git"
@@ -19,5 +25,11 @@
         "colorize"
       ];
     };
+
+    plugins = with pkgs; [
+      zsh-nix-shell
+    ];
   };
+
+  home.file.".oh-my-zsh/custom/themes/agnoster-nix.zsh-theme".source = agnoster-nix-theme;
 }
