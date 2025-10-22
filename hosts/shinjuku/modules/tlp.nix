147814c40b3e279f2c3783132d89dfa0820e052f
JulWas797 <51297298+JulWas797@users.noreply.github.com>
2025-10-22 22:26:25 +0200

    no ppd

--- hosts/shinjuku/modules/tlp.nix
+++ hosts/shinjuku/modules/tlp.nix
@@ -1,14 +1,18 @@
-{ ... }: 
+{ lib, ... }: 
 
 {
-  services.tlp = {
-    enable = true;
+  services = {
+    tlp = {
+      enable = true;
 
-    settings = { 
-      START_CHARGE_THRESH_BAT0 = 80;
-      START_CHARGE_THRESH_BAT1 = 85;
-      STOP_CHARGE_THRESH_BAT0 = 85;
-      STOP_CHARGE_THRESH_BAT1 = 90; 
+      settings = { 
+        START_CHARGE_THRESH_BAT0 = 80;
+        START_CHARGE_THRESH_BAT1 = 85;
+        STOP_CHARGE_THRESH_BAT0 = 85;
+        STOP_CHARGE_THRESH_BAT1 = 90; 
+      };
     };
+
+    power-profiles-daemon.enable = lib.mkForce false;
   };
 }
