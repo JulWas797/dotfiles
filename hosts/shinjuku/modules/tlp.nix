b26cf8adcf949345e98586c5212221f6eb63171a
JulWas797 <51297298+JulWas797@users.noreply.github.com>
2025-10-23 00:38:04 +0200

    TLP

--- hosts/shinjuku/modules/tlp.nix
+++ hosts/shinjuku/modules/tlp.nix
@@ -6,6 +6,9 @@
       enable = true;
 
       settings = { 
+        CPU_BOOST_ON_BAT=0;
+        CPU_HWP_DYN_BOOST_ON_BAT=0;
+
         START_CHARGE_THRESH_BAT0 = 80;
         START_CHARGE_THRESH_BAT1 = 85;
         STOP_CHARGE_THRESH_BAT0 = 85;
