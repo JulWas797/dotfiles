c7f7a83ab63b5043527befbacad841f9641e764d
JulWas797 <51297298+JulWas797@users.noreply.github.com>
2025-12-22 01:02:45 +0100

    Flake fix

--- flake/nyanix/default.nix
+++ flake/nyanix/default.nix
@@ -10,6 +10,7 @@ inputs.nixpkgs.lib.nixosSystem {
     ../../modules/base/shared
     ../../modules/base/nyanix
     inputs.home-manager.nixosModules.home-manager
+    inputs.sops-nix.nixosModules.sops
 
     {      
       nixpkgs.overlays = [ inputs.nix-vscode-extensions.overlays.default ];
@@ -24,6 +25,7 @@ inputs.nixpkgs.lib.nixosSystem {
 
         sharedModules = [
           inputs.plasma-manager.homeModules.plasma-manager
+          inputs.sops-nix.homeManagerModules.sops
         ];
 
         users.jwas = ../../homes/nyanix;
