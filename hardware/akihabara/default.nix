134e2493a6ce06bba1d322423799ca2e1c89eac1
JulWas797 <51297298+JulWas797@users.noreply.github.com>
2026-01-01 20:02:42 +0100

    name

--- hardware/akihabara/default.nix
+++ hardware/akihabara/default.nix
@@ -33,7 +33,7 @@
     "/multimedia" = { 
       device = "/dev/disk/by-uuid/e2c6a0d6-ae0f-4bf2-9c45-a1b7b249f18f";
       fsType = "btrfs";
-      options = [ "subvol=@media" "noatime" ];
+      options = [ "subvol=@multimedia" "noatime" ];
     };
 
     "/boot" = { 
