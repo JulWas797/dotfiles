3345dc540149ad55c3bc3b53fec79df4d0905f50
JulWas797 <51297298+JulWas797@users.noreply.github.com>
2026-01-02 21:20:17 +0100

    reposilite first setup

--- modules/base/akihabara/maven/default.nix
+++ modules/base/akihabara/maven/default.nix
@@ -6,6 +6,7 @@
   config = lib.mkIf config.modules.maven.enable {
     services.reposilite = {
       enable = true;
+      extraArgs = [ "--token" "name:tempsecrettoken" ];
 
       plugins = with pkgs.reposilitePlugins; [
         groovy
